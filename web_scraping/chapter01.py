5a021a69eefe705e964c3b0b6fa6d54c81827715
KIMSANGHYUN <blusky0910@gmail.com>
2020-10-12 08:49:23 +0900

    web scraping 폴더 추가

---
+++ web_scraping/chapter01.py
@@ -0,0 +1,34 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
+from bs4 import BeautifulSoup
+
+import os
+os.environ["HTTP_PROXY"] = "http://70.10.15.10:8080"
+os.environ["HTTPS_PROXY"] = "http://70.10.15.10:8080"
+os.environ["PYTHONHTTPSVERIFY"] = "0"
+
+def getTitle(url):
+    try:
+        html=urlopen(url)
+    except HTTPError as e:
+        print(e)
+    except URLError as e:
+        print("Server could not be found")
+    else:
+        print("It worked")
+
+    try:
+        bs=BeautifulSoup(html.read(), 'html.parser')
+        title=bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title=getTitle("http://www.pythonscraping.com/pages/page1.html")
+
+if title == None:
+    print('title could not be found')
+else:
+    print(title)    
