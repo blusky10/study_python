e6fdde4d17a17d2e10f93e5199127b7cb2306c41
KIMSANGHYUN <blusky0910@gmail.com>
2020-10-12 09:43:21 +0900

    파일 수정

--- firstPython/web/learning_log/learning_log/settings.py
+++ firstPython/web/learning_log/learning_log/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e5s+clld(bb2++es!%lvl5jjjqqnzuiij-u*ks%y$k7p815mwp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
