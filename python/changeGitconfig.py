95bb34a987d67205c68f1bb01f5c581805c0367b
sanghyun-kim <sanghyun0910.kim@samsung.com>
2021-08-25 09:40:50 +0900

    불필요 파일 삭제

--- python/changeGitconfig.py
+++
@@ -1,9 +0,0 @@
-def changeStringInFile(filePath, orgValue, newValue):
-    with open(filePath, "r+") as file_object:
-        fileContents = file_object.read()
-        newFileContents = fileContents.replace(orgValue, newValue)   
-        file_object.seek(0)
-        file_object.write(newFileContents)
-        file_object.close()
-
-changeStringInFile("c:\\Users\\SDS\\.gitconfig", "#", "")
